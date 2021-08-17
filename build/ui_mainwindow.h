7aed45421941db535fe26e942b9cde549e5993d6
MARKMENTAL <marky611@gmail.com>
2021-08-16 20:40:06 -0400

    Pre-Compiled Build

---
+++ build/ui_mainwindow.h
@@ -0,0 +1,118 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionAbout;
+    QAction *btnabt;
+    QWidget *centralwidget;
+    QLabel *lbl0;
+    QComboBox *cmb0;
+    QPushButton *btn0;
+    QLabel *lbl1;
+    QMenuBar *menubar;
+    QMenu *menuHelp;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->setWindowModality(Qt::NonModal);
+        MainWindow->resize(401, 372);
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
+        MainWindow->setLayoutDirection(Qt::LeftToRight);
+        actionAbout = new QAction(MainWindow);
+        actionAbout->setObjectName(QString::fromUtf8("actionAbout"));
+        btnabt = new QAction(MainWindow);
+        btnabt->setObjectName(QString::fromUtf8("btnabt"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        lbl0 = new QLabel(centralwidget);
+        lbl0->setObjectName(QString::fromUtf8("lbl0"));
+        lbl0->setGeometry(QRect(60, 10, 271, 141));
+        QFont font;
+        font.setPointSize(18);
+        font.setBold(true);
+        font.setWeight(75);
+        lbl0->setFont(font);
+        lbl0->setScaledContents(true);
+        lbl0->setAlignment(Qt::AlignCenter);
+        lbl0->setWordWrap(true);
+        cmb0 = new QComboBox(centralwidget);
+        cmb0->setObjectName(QString::fromUtf8("cmb0"));
+        cmb0->setGeometry(QRect(30, 140, 87, 34));
+        btn0 = new QPushButton(centralwidget);
+        btn0->setObjectName(QString::fromUtf8("btn0"));
+        btn0->setGeometry(QRect(270, 140, 111, 36));
+        btn0->setFlat(false);
+        lbl1 = new QLabel(centralwidget);
+        lbl1->setObjectName(QString::fromUtf8("lbl1"));
+        lbl1->setGeometry(QRect(130, 180, 131, 131));
+        lbl1->setScaledContents(true);
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 401, 32));
+        menuHelp = new QMenu(menubar);
+        menuHelp->setObjectName(QString::fromUtf8("menuHelp"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuHelp->menuAction());
+        menuHelp->addAction(btnabt);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "Qt Distro Demo by MARKMENTAL", nullptr));
+        actionAbout->setText(QCoreApplication::translate("MainWindow", "About", nullptr));
+        btnabt->setText(QCoreApplication::translate("MainWindow", "About", nullptr));
+        lbl0->setText(QCoreApplication::translate("MainWindow", "TextLabel", nullptr));
+        btn0->setText(QCoreApplication::translate("MainWindow", "Get Distro Info", nullptr));
+        lbl1->setText(QString());
+        menuHelp->setTitle(QCoreApplication::translate("MainWindow", "Help", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
